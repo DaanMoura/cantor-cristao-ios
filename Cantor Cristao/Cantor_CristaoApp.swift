8f3a378e38809a13043aade1e14e84f4ec0667b5
Daniel Moura <daniel.lucio.moura@gmail.com>
2023-04-07 21:16:58 -0300

    Initial Commit

---
+++ Cantor Cristao/Cantor_CristaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cantor_CristaoApp.swift
+//  Cantor Cristao
+//
+//  Created by Daniel Moura on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Cantor_CristaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
